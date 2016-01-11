d57698526ec0390a56b9ecf7688009ce9a4e658e
only1felton <only1felton@gmail.com>
2016-01-10 22:20:19 -0500

    added GitHub search button with results

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -55,6 +55,8 @@
     {
          /*global activate_subpage */
          activate_subpage("#uib_page_1"); 
+         /*close sidebars*/
+         uib_sb.close_all_sidebars();
     });
     
         /* button  Your Courses */
@@ -82,9 +84,62 @@
     $(document).on("click", "#search-button", function(evt)
     {
         /* your code goes here */ 
-        //hold
+        var resultList=$('#resultList');
+        var searchPhrase=$('#search-input').val();
+        var gitHubSearch="https://api.github.com/search/repositories?q=";
+        
+        if (searchPhrase) {
+             resultList.text("Performing Search...");
+             gitHubSearch += encodeURIComponent(searchPhrase);
+            $.get(gitHubSearch)
+            .success(function(r){
+                displayResults(r.items, resultList); 
+            });
+             
+        }
+
+       
+    });
+    
+        /* button  #clear-btn */
+    $(document).on("click", "#clear-btn", function(evt)
+    {
+        /* your code goes here */ 
+        var resultList=$('#resultList');
+        clearResults(resultList);
+    });
+    
+        /* button  Drills */
+    $(document).on("click", ".uib_w_32", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#dialog-page"); 
     });
     
     }
  document.addEventListener("app.Ready", register_event_handlers, false);
 })();
+
+function displayResults(results, resultList){
+    //empty results
+    resultList.empthy();
+    
+    //loop through result items and get information, then append to resultList
+    $.each(results, function(i, item){
+            //create new result entry from data
+           var newResult = $("<div class='result'>" +
+                            "<div class='title'>" + item.name + "</div>" +
+                            "<div>Language: " + item.language + "</div>" +
+                            "<div>Owner: " + item.owner.login + "</div>" +
+                             "<div>URL: <a href='" + item.html_url + "'>" + item.html_url + "</a></div>" +
+                            "</div>");
+
+            //append new result entry to resultList
+            resultList.append(newResult);
+    });
+
+}
+
+function clearResults(resultList){
+    resultList.empty();
+}
